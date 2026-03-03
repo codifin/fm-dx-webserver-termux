f0da4a5ca2f63d888a1f0175837f887990e1e2c9
Kodi Kiselev <217178086+codifin@users.noreply.github.com>
2026-03-03 21:36:33 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,15 +9,26 @@ echo "Installing Node.js dependencies..."
 npm install --omit=optional
 
 echo "Creating dummy Node modules..."
-# Shim for ffmpeg-static to use Termux system ffmpeg
 mkdir -p node_modules/ffmpeg-static
 echo "module.exports = '$(which ffmpeg)';" > node_modules/ffmpeg-static/index.js
 echo '{"name": "ffmpeg-static", "version": "5.2.0", "main": "index.js"}' > node_modules/ffmpeg-static/package.json
 
-# Shim for serialport to prevent hardware access errors
 mkdir -p node_modules/serialport/dist
 mkdir -p node_modules/@serialport/parser-byte-length
-echo "module.exports = { SerialPort: function() { this.on = () => {}; this.write = () => {}; this.pipe = () => ({ on: () => {} }); } };" > node_modules/serialport/dist/index.js
+
+cat <<EOT > node_modules/serialport/dist/index.js
+module.exports = {
+  SerialPort: function() {
+    this.on = () => {};
+    this.write = () => {};
+    this.pipe = () => ({ on: () => {} });
+  },
+  list: function() {
+    return Promise.resolve([]);
+  }
+};
+EOT
+
 echo "module.exports = { ByteLengthParser: function() {} };" > node_modules/@serialport/parser-byte-length/index.js
 echo '{"name": "serialport", "version": "1.0.0", "main": "dist/index.js"}' > node_modules/serialport/package.json
 echo '{"name": "@serialport/parser-byte-length", "version": "1.0.0", "main": "index.js"}' > node_modules/@serialport/parser-byte-length/package.json
@@ -25,4 +36,4 @@ echo '{"name": "@serialport/parser-byte-length", "version": "1.0.0", "main": "in
 echo "Setting up library permissions..."
 find . -name "librdsparser.so" -exec chmod +x {} \;
 
-echo "Setup complete. You can now run your start script."
+echo "Setup complete. You can now run your start script with ./start.sh."
