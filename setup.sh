1dba758f54758b14143711e8e2067874ff091c0b
Kodi Kiselev <217178086+codifin@users.noreply.github.com>
2026-03-03 21:49:24 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,13 +9,16 @@ echo "Installing Node.js dependencies..."
 npm install --omit=optional
 
 echo "Creating dummy Node modules..."
+# FFmpeg shim
 mkdir -p node_modules/ffmpeg-static
-echo "module.exports = '$(which ffmpeg)';" > node_modules/ffmpeg-static/index.js
+echo "module.exports = '$PREFIX/bin/ffmpeg';" > node_modules/ffmpeg-static/index.js
 echo '{"name": "ffmpeg-static", "version": "5.2.0", "main": "index.js"}' > node_modules/ffmpeg-static/package.json
 
+# SerialPort and Parser shims
 mkdir -p node_modules/serialport/dist
 mkdir -p node_modules/@serialport/parser-byte-length
 
+# Main serialport shim with .list() method
 cat <<EOT > node_modules/serialport/dist/index.js
 module.exports = {
   SerialPort: function() {
@@ -29,11 +32,12 @@ module.exports = {
 };
 EOT
 
-echo "module.exports = { ByteLengthParser: function() {} };" > node_modules/@serialport/parser-byte-length/index.js
-echo '{"name": "serialport", "version": "1.0.0", "main": "dist/index.js"}' > node_modules/serialport/package.json
+# Parser-byte-length shim
+echo "module.exports = { ByteLengthParser: function() { this.on = () => {}; } };" > node_modules/@serialport/parser-byte-length/index.js
 echo '{"name": "@serialport/parser-byte-length", "version": "1.0.0", "main": "index.js"}' > node_modules/@serialport/parser-byte-length/package.json
+echo '{"name": "serialport", "version": "1.0.0", "main": "dist/index.js"}' > node_modules/serialport/package.json
 
 echo "Setting up library permissions..."
 find . -name "librdsparser.so" -exec chmod +x {} \;
 
-echo "Setup complete. You can now run your start script with ./start.sh."
+echo "Setup complete. You can now run your start script."
