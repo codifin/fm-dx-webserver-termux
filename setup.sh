c96b02c68acdc6785aa8bf2dca322b56d571b0c6
Kodi Kiselev <217178086+codifin@users.noreply.github.com>
2026-03-03 22:07:27 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,36 +8,12 @@ chmod +x $PREFIX/bin/udevadm $PREFIX/bin/arecord
 echo "Installing Node.js dependencies..."
 npm install --omit=optional
 
-echo "Creating dummy Node modules..."
-# FFmpeg shim
+echo "Configuring FFmpeg for Termux..."
 mkdir -p node_modules/ffmpeg-static
 echo "module.exports = '$PREFIX/bin/ffmpeg';" > node_modules/ffmpeg-static/index.js
 echo '{"name": "ffmpeg-static", "version": "5.2.0", "main": "index.js"}' > node_modules/ffmpeg-static/package.json
 
-# SerialPort and Parser shims
-mkdir -p node_modules/serialport/dist
-mkdir -p node_modules/@serialport/parser-byte-length
-
-# Main serialport shim with .list() method
-cat <<EOT > node_modules/serialport/dist/index.js
-module.exports = {
-  SerialPort: function() {
-    this.on = () => {};
-    this.write = () => {};
-    this.pipe = () => ({ on: () => {} });
-  },
-  list: function() {
-    return Promise.resolve([]);
-  }
-};
-EOT
-
-# Parser-byte-length shim
-echo "module.exports = { ByteLengthParser: function() { this.on = () => {}; } };" > node_modules/@serialport/parser-byte-length/index.js
-echo '{"name": "@serialport/parser-byte-length", "version": "1.0.0", "main": "index.js"}' > node_modules/@serialport/parser-byte-length/package.json
-echo '{"name": "serialport", "version": "1.0.0", "main": "dist/index.js"}' > node_modules/serialport/package.json
-
 echo "Setting up library permissions..."
 find . -name "librdsparser.so" -exec chmod +x {} \;
 
-echo "Setup complete. You can now run your start script."
+echo "Setup complete. USB support has been removed from shims."
