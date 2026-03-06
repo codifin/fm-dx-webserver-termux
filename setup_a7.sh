08cebb2508955ac8007d64fcda13b1ad3312221b
Kodi Kiselev <217178086+codifin@users.noreply.github.com>
2026-03-06 20:05:33 +0500

    Create setup_a7.sh

---
+++ setup_a7.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Creating system shims..."
+echo -e "#!/bin/sh\nexit 0" > $PREFIX/bin/udevadm
+echo -e "#!/bin/sh\nexit 0" > $PREFIX/bin/arecord
+chmod +x $PREFIX/bin/udevadm $PREFIX/bin/arecord
+
+echo "Installing Node.js dependencies..."
+export MAKEFLAGS="-j1"
+export CFLAGS="-I$PREFIX/include"
+export CXXFLAGS="-I$PREFIX/include"
+npm install --build-from-source
+
+echo "Configuring FFmpeg for Termux..."
+mkdir -p node_modules/ffmpeg-static
+echo "module.exports = '$PREFIX/bin/ffmpeg';" > node_modules/ffmpeg-static/index.js
+echo '{"name": "ffmpeg-static", "version": "5.2.0", "main": "index.js"}' > node_modules/ffmpeg-static/package.json
+
+echo "Setting up library permissions..."
+find . -name "librdsparser.so" -exec chmod +x {} \;
+
+echo "Setup complete. USB support has been removed from shims."
