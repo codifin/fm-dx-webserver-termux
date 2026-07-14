f1b5fd2e36e685fa3fec22d5d782230455cdc53e
Kodi Kiselev <217178086+codifin@users.noreply.github.com>
2026-07-14 21:57:27 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,18 +5,26 @@ echo -e "#!/bin/sh\nexit 0" > $PREFIX/bin/udevadm
 echo -e "#!/bin/sh\nexit 0" > $PREFIX/bin/arecord
 chmod +x $PREFIX/bin/udevadm $PREFIX/bin/arecord
 
-echo "Installing Node.js dependencies..."
-export MAKEFLAGS="-j1"
-export CFLAGS="-I$PREFIX/include"
-export CXXFLAGS="-I$PREFIX/include"
-npm install --omit=optional
-
-echo "Configuring FFmpeg for Termux..."
+echo "Linking pre-installed Node.js dependencies..."
 mkdir -p node_modules/ffmpeg-static
 echo "module.exports = '$PREFIX/bin/ffmpeg';" > node_modules/ffmpeg-static/index.js
 echo '{"name": "ffmpeg-static", "version": "5.2.0", "main": "index.js"}' > node_modules/ffmpeg-static/package.json
 
-echo "Setting up library permissions..."
+echo "Setting up library and binary permissions..."
 find . -name "librdsparser.so" -exec chmod +x {} \;
 
-echo "Setup complete. USB support has been removed from shims."
+if [ -d "node_modules/koffi" ]; then
+    chmod -R +x node_modules/koffi 2>/dev/null
+fi
+
+if [ -d "bin" ]; then
+    chmod +x bin/* 2>/dev/null
+fi
+if [ -d "server/bin" ]; then
+    chmod +x server/bin/* 2>/dev/null
+fi
+
+echo "------------------------------------------------"
+echo "Setup complete!"
+echo "You can now start the server using: ./start.sh"
+echo "------------------------------------------------"
